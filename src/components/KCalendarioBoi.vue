6d0e8d597d8222ea8c4875534ec6027b8e5b06b4
jardelf91 <jardelf91@gmail.com>
2024-06-09 13:26:59 -0400

    Feat - modal para mostrar dados do eventos

--- src/components/KCalendarioBoi.vue
+++ src/components/KCalendarioBoi.vue
@@ -1,14 +1,58 @@
 <template>
   <div class="calendar-container">
     <FullCalendar :options="calendarOptions" />
+
+    <q-dialog v-model="isModalOpen" persistent>
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">{{ eventDetails.title }}</div>
+          <div>{{ eventDetails.start }}</div>
+          <br />
+          <div>{{ eventDetails.description }}</div>
+          <div v-if="eventDetails.location" class="location-section">
+            <q-icon name="place" color="primary" />
+            <div class="location-text">
+              <a :href="`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(eventDetails.location)}`"
+                target="_blank" class="location-link">
+                <div class="location-name">{{ eventDetails.location.split(',')[0] }}</div>
+                <div class="location-address">{{ eventDetails.location }}</div>
+              </a>
+            </div>
+          </div>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="Fechar" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
-
 <script setup lang="ts">
 import { ref } from 'vue'
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import googleCalendarPlugin from '@fullcalendar/google-calendar'
+import { QIcon } from 'quasar'
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
+
+const isModalOpen = ref(false)
+const eventDetails = ref({
+  title: '',
+  start: '',
+  end: '',
+  description: '',
+  location: ''
+})
+
+const formatStartEnd = (startStr: string, endStr: string) => {
+  const startDate = new Date(startStr)
+  const endDate = new Date(endStr)
+  const formattedStartDate = format(startDate, "PPPP ', das' HH:mm", { locale: ptBR })
+  const formattedEndTime = format(endDate, 'HH:mm', { locale: ptBR })
+  return `${formattedStartDate} às ${formattedEndTime}`
+}
 
 const calendarOptions = ref({
   plugins: [dayGridPlugin, googleCalendarPlugin],
@@ -33,12 +77,27 @@ const calendarOptions = ref({
   locale: 'pt-br',
   themeSystem: 'standard',
   eventDisplay: 'block',
-})
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  eventClick: (info: any) => {
+    // Previne a navegação para o Google Calendar
+    info.jsEvent.preventDefault()
 
+    eventDetails.value = {
+      title: info.event.title,
+      start: formatStartEnd(info.event.startStr, info.event.endStr),
+      description: info.event.extendedProps.description || 'Sem descrição',
+      location: info.event.extendedProps.location || 'Sem localização',
+      end: info.event.endStr || '' // Adiciona end apenas se estiver definido
+    }
+
+    isModalOpen.value = true
+  }
+})
 </script>
 
+
+
 <style scoped>
-/* Adicione estilos personalizados aqui, se necessário */
 .calendar-container {
   background-color: #e1ac26;
   color: black;
@@ -50,13 +109,60 @@ const calendarOptions = ref({
   --fc-button-hover-border-color: back;
   --fc-button-active-bg-color: black;
   --fc-button-active-border-color: back;
-  /* height: auto;
-  width: 100%; */
 }
 
 .gcal-event {
   background-color: #1e90ff;
   border-color: #1e90ff;
   color: black;
+}
+
+/* Novos estilos para garantir que os eventos não sejam cortados */
+.fc .fc-event-title,
+.fc .fc-event-time {
+  white-space: normal;
+  /* Permite quebra de linha */
+  word-wrap: break-word;
+  /* Quebra palavras longas */
+}
+
+.fc .fc-daygrid-event {
+  height: auto !important;
+  /* Garante que o container do evento ajuste a altura automaticamente */
+}
+
+/* Ajuste para telas pequenas */
+@media (max-width: 768px) {
+
+  .fc .fc-event-title,
+  .fc .fc-event-time {
+    font-size: 0.875rem;
+    /* Reduz o tamanho da fonte */
+  }
+}
+
+.location-section {
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+}
+
+.location-text {
+  margin-left: 10px;
+}
+
+.location-link {
+  text-decoration: none;
+  color: inherit;
+}
+
+.location-name {
+  font-weight: bold;
+  color: var(--q-color-primary);
+}
+
+.location-address {
+  font-size: 0.875rem;
+  color: var(--q-color-secondary);
 }
 </style>
